c0789425da8e4455db7fe00fdc1bc86c288bec62
IconicLuck <uhasib959@gmail.com>
2023-03-29 12:40:16 +0600

    added meal designs for each meal

--- src/components/Meal/Meal.jsx
+++ src/components/Meal/Meal.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Meal.css'
 
 const Meal = (props) => {
     console.log(props)
-    const {strArea, strMealThumb, strInstructions} = props.meal
+    const { strArea, strMealThumb, strCategory, strMeal } = props.meal
     return (
-        <div>
-            <h1>This is Meal</h1>
+        <div className='meal-container rounded-xl'>
+            <div className='p-5'>
+                <img className='rounded-xl mb-1' src={strMealThumb} alt="" />
+                <h2 className='text-2xl font-semibold mb-3'>{strMeal}</h2>
+                <h4 className='text-xl font-semibold'>Place: {strArea}</h4>
+                <h4 className='text-xl font-semibold mb-5'>Category: {strCategory}</h4>
+            </div>
+            <button className='bg-[#FFE0B3] hover:bg-orange-500 ease-in-out duration-200 w-full h-12 btn-add'>Add to List</button>
         </div>
     );
 };
--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Meal from '../Meal/Meal';
+import './Meals.css'
 
 const Shop = () => {
     const [meals, setMeals] = useState([])
     useEffect(() => {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-        .then(res => res.json())
-        .then(data => setMeals(data.meals))
+            .then(res => res.json())
+            .then(data => setMeals(data.meals))
     }, [])
     return (
-        <div>
-            {
-                meals.map(meal => <Meal key={meal.idMeal} meal={meal}></Meal>)
-            }
+        <div className='meals-container'>
+            <div className='grid grid-cols-3 gap-10 p-10'>
+                {
+                    meals.map(meal => <Meal key={meal.idMeal} meal={meal}></Meal>)
+                }
+            </div>
+            <div>
+                <h1 className='text-4xl font-semibold'>Selected Meals: </h1>
+            </div>
         </div>
     );
 };
